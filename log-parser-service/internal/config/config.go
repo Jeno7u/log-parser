71b0d69517c760a3ad722ff32cb7bdd0808e0a0d
Mironov Boris <lboris201@yandex.ru>
2026-05-14 10:22:28 +0300

    Added config file, added .env

--- log-parser-service/internal/config/config.go
+++ log-parser-service/internal/config/config.go
@@ -1,1 +1,44 @@
 package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/lpernett/godotenv"
+)
+
+type Config struct {
+	PostgresConnString string
+	Port               string
+	LogLevel           string
+	DataDir            string
+}
+
+func InitConfig() Config {
+	_ = godotenv.Load(".env")
+	_ = godotenv.Load("../.env")
+
+	postgresConnString := fmt.Sprintf("postgres://%s:%s@%s:5432/%s?sslmode=disable",
+		getEnv("POSTGRES_USER", "postgres"),
+		getEnv("POSTGRES_PASSWORD", "password"),
+		getEnv("POSTGRES_HOST", "postgres"),
+		getEnv("POSTGRES_DB", "db"),
+	)
+
+	return Config{
+		PostgresConnString: postgresConnString,
+		Port:               getEnv("PORT", "8080"),
+		LogLevel:           getEnv("LOG_LEVEL", "info"),
+		DataDir:            getEnv("DATA_DIR", "/app/data"),
+	}
+}
+
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+
+	log.Printf("cant find env by key: %v, using: %v", key, fallback)
+	return fallback
+}
