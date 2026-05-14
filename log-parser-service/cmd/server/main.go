6a4b918ba11cff14e31e10e1d0c6dffbb7cc5b13
Mironov Boris <lboris201@yandex.ru>
2026-05-14 20:08:50 +0300

    Small work on main.go

--- log-parser-service/cmd/server/main.go
+++ log-parser-service/cmd/server/main.go
@@ -1,19 +1,44 @@
 package main
 
 import (
+	"context"
+	"log/slog"
+	"os"
+
 	"github.com/Jeno7u/log-parser/internal/config"
 	"github.com/Jeno7u/log-parser/internal/db"
+	"github.com/Jeno7u/log-parser/internal/handlers"
+	apphttp "github.com/Jeno7u/log-parser/internal/http"
 	"github.com/Jeno7u/log-parser/internal/repository"
+	"github.com/Jeno7u/log-parser/internal/service"
 )
 
 func main() {
-	// create config struct
-	config := config.InitConfig()
+	// creating logger and config setup
+	log := config.NewLogger()
+	config := config.InitConfig(log)
 
-	// setup db
+	// db setup
 	pool := db.NewPostgresPool(config.PostgresConnString)
 	defer pool.Close()
 
-	// create repository, service and other stuff
+	// creating repositories
 	logRepository := repository.NewLogRepository(pool)
+	nodeRepository := repository.NewNodeRepository(pool)
+	portRepository := repository.NewPortRepository(pool)
+	parseService := service.NewParse(logRepository, nodeRepository, portRepository)
+
+	// creating handlers
+	parseHandler := handlers.NewParse(parseService, log)
+	topologyHandler := handlers.NewTopology(logRepository, nodeRepository, portRepository, log)
+	logHandler := handlers.NewLog(logRepository, log)
+	nodeHandler := handlers.NewNode(nodeRepository, log)
+	portHandler := handlers.NewPort(portRepository, log)
+
+	// creating server instance and running it
+	server := apphttp.Setup(config, log, parseHandler, topologyHandler, logHandler, nodeHandler, portHandler)
+	if err := server.Run(context.Background()); err != nil {
+		log.Error("server stopped with error", slog.String("error", err.Error()))
+		os.Exit(1)
+	}
 }
