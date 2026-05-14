c76e59055ad58281f698576726ccc035066fa99a
Mironov Boris <lboris201@yandex.ru>
2026-05-14 11:00:36 +0300

    Added db setupping in main

--- log-parser-service/cmd/server/main.go
+++ log-parser-service/cmd/server/main.go
@@ -1,1 +1,19 @@
-package server
+package main
+
+import (
+	"github.com/Jeno7u/log-parser/internal/config"
+	"github.com/Jeno7u/log-parser/internal/db"
+	"github.com/Jeno7u/log-parser/internal/repository"
+)
+
+func main() {
+	// create config struct
+	config := config.InitConfig()
+
+	// setup db
+	pool := db.NewPostgresPool(config.PostgresConnString)
+	defer pool.Close()
+
+	// create repository, service and other stuff
+	logRepository := repository.NewLogRepository(pool)
+}
